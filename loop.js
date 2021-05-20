e909e1769c054e19550a7ac254c92a92cf7ec5ee
adepoju2006 <olanrewajuabdulrazaq5@gmail.com>
2021-05-20 16:00:03 +0100

    first commit

---
+++ loop.js
@@ -0,0 +1,38 @@
+// var fruits = ["Mango", "Cucumber", "Pineapple", "Apple", "Orange"];
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log("For loop", fruits[i]);
+// }
+
+// // for in loop
+// for (j in fruits) {
+//   console.log("For In loop", fruits[j]);
+// }
+
+var A = [
+  "Adepoju",
+  "Ayomide",
+  "Adebayo",
+  "Network",
+  "OdunAyo",
+  "Omolara",
+  "Ademide",
+  "Okunlawo",
+  "Sukurat",
+  "Nicholas",
+];
+var B = [
+  "Nicolas",
+  "Ayodara",
+  "Ademide",
+  "Sukurat",
+  "Chineye",
+  "Atomide",
+  "Adebayo",
+  "Console",
+  "Opeyemi",
+  "Adepoju",
+];
+
+for (j in A && B) {
+  console.log(B[j] + " " + " " + " " + " " + " " + A[j]);
+}
